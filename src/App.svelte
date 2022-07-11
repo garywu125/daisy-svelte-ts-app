e5117b0a5655b5a9fb9cdb0d4102c3095474a069
garywu125 <garywu125@gmail.com>
2022-07-11 15:53:07 +0800

    second commit

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,24 @@
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  
 <button class="btn">Button</button>
 <button class="btn btn-primary">Button</button>
 <button class="btn btn-secondary">Button</button>
 <button class="btn btn-accent">Button</button>
 <button class="btn btn-ghost">Button</button>
 <button class="btn btn-link">Button</button>
+
+<div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
 
   <Counter />
 
