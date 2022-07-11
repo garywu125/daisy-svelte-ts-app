bd42090b0e328d1c3f9f7daa128769e1264a3a2f
garywu125 <garywu125@gmail.com>
2022-07-11 15:40:12 +0800

    first commit after repository has created

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1>Hello Typescript!</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+<button class="btn">Button</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
